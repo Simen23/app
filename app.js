d0fe406ed2be5edb284e39bf25b229682d6b0ad8
Simen23 <95966636+Simen23@users.noreply.github.com>
2023-01-28 17:30:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function first() {
+    alert(`Будет ${50+100}`)
+}
+function second () {
+    let age = prompt('Введи свой возраст: ')
+    if (age < 18) {
+    alert('Ты ещё маленький (ая)')
+   } else{
+    alert('Вход разрешён')
+    }
+  }
+
+  function third() {
+    document.getElementById ('zp').innerHTML = '1000$'; 
+    }
